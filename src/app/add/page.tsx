1fbfe2f5993f6932c676f72bc235c9921644ed8f
Mrugank93 <mrugank@elemensissoftech.com>
2024-08-21 17:19:19 +0530

    feat(design ): responsive design

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -23,10 +23,15 @@ export default function Page() {
 
   return (
     <div className="flex min-h-screen flex-col justify-center items-center px-6 py-12 lg:px-8 relative overflow-hidden bg-[#093545]">
-      <div className="max-w-6xl w-full p-12">
-        <h1 className="text-5xl font-bold mb-8 text-white">Create a new movie</h1>
-        <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row gap-8">
-          <div className="w-full sm:w-1/2 flex justify-center items-center border-2 border-dashed border-white h-96 rounded-lg bg-[#224957]">
+      <div className="max-w-6xl w-full p-4 sm:p-12">
+        <h1 className="text-3xl sm:text-5xl font-bold mb-8 text-white text-center sm:text-left">
+          Create a new movie
+        </h1>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col sm:flex-row gap-8"
+        >
+          <div className="w-full sm:w-1/2 flex justify-center items-center border-2 border-dashed border-white h-60 sm:h-96 rounded-lg bg-[#224957]">
             <label className="flex flex-col justify-center items-center w-full h-full cursor-pointer">
               {imagePreview ? (
                 <img
@@ -62,10 +67,10 @@ export default function Page() {
               placeholder="Publishing Year"
               value={year}
               onChange={(e) => setYear(e.target.value)}
-              className="bg-[#224957] text-white px-5 py-3 rounded-lg outline-none text-lg w-1/2"
+              className="bg-[#224957] text-white px-5 py-3 rounded-lg outline-none text-lg w-full sm:w-3/4 md:w-1/2 lg:w-1/2"
             />
 
-            <div className="flex justify-center gap-4">
+            <div className="flex justify-center gap-4 mt-8">
               <button
                 type="button"
                 className="bg-transparent text-white border border-white px-6 py-3 rounded-lg hover:bg-white hover:text-gray-800 transition text-lg"
@@ -74,7 +79,7 @@ export default function Page() {
               </button>
               <button
                 type="submit"
-                className="bg-[#2BD17E] text-white px-6 py-3 rounded-lg hover:bg-[#2BD17E] transition text-lg"
+                className="bg-[#2BD17E] text-white px-6 py-3 rounded-lg hover:bg-[#28a76d] transition text-lg"
               >
                 Submit
               </button>
